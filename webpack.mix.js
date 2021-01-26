92c41930edb80cfa4f46f7b4a6687a1955f9b98e
Pedro Protest <pedro.protest@gmail.com>
2021-01-26 03:43:06 +0000

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,18 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist').setPublicPath('dist');
+mix
+  .js("assets/js/app.js", "build")
+  .postCss('assets/src/sass/main.sass', 'css', [
+    require('tailwindcss'),
+    require('postcss-nested')
+  ])
+  .setPublicPath("assets/build")
+  .setResourceRoot("./")
+  .browserSync({
+    proxy: "localhost/dotfolio",
+    files: ["./**/*.php", "./assets/build/*.js", "./assets/build/*.css"]
+  });
+
+.options({
+	processCssUrls: false
+});
